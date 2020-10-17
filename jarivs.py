1653a73fe43a62ef2448e60c9fdf4013991ed33f
arhaantulsyan <72146157+arhaantulsyan@users.noreply.github.com>
2020-10-17 17:55:39 +0530

    Update jarivs.py

--- jarivs.py
+++ jarivs.py
@@ -135,6 +135,35 @@ if __name__ == "__main__":
             pypath="C:\\Program Files\\JetBrains\\PyCharm Community Edition 2020.1\\bin\\pycharm64.exe"
             os.startfile(pypath)
 
-        
+            elif 'email to me' in query:
+            try:
+                speak("What Should I Send!")
+                content = takeCommand()
+                to = "pratikkumar753@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry Sir, The Email not send")
+
+        elif 'exit' in query:
+            print("Computer: Quitting sir,Thank you for your time.")
+            speak("Quitting sir,Thank you for your time.")
+            print(exit())
+
+        elif 'introduce yourself' in query:
+            temp1="hii I am jarvis. I am a Assistant. Which is Develop By, Prateek Kumar"
+            speak(temp1)
+
+
+        elif 'hello' in query:
+            temp3="hello,Sir How may I help you"
+            print(temp3)
+            speak(temp3)
+
+                    
+         elif 'quit' or 'exit' in query:
+             speak("Thank you sir")
+              print(exit())
         
     
